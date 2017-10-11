3dd4b3246993391ffee4b16737b313bcc9963c63
kravc4enkoserg <kravc4enkoserg@gmail.com>
2017-10-11 19:31:03 +0300

    доделаны страници index, details, personal-data

--- app/js/init.js
+++ app/js/init.js
@@ -36,25 +36,58 @@ $( function() {
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["01", "02", '03', '04', '05', '06', '07', '08', '09', '10'],
-                datasets: [
-                    {
-                        label: '# of Votes',
-                        data: [100, 190, 120, 200, 210, 150, 175, 143, 110, 130],
-                        backgroundColor: [
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                            'rgba(128, 191, 202, 1)',
-                            'rgba(255, 154, 56, 1)',
-                        ]
-                    }
-                ]
+                labels: ["01", "02", "04", "05", "06", "07", "08", "09", "10"],
+                datasets: [{
+                    label: 'Signups',
+                    data: [350, 200, 250, 350, 400, 375, 275, 250, 275],
+                    backgroundColor: [
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                        'rgba(128, 191, 202, 1)',
+                    ],
+    
+                },
+                {
+                    label: 'FTD',
+                    data: [425, 180, 300, 275, 450, 100, 200, 500, 300],
+                    backgroundColor: [
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                        'rgba(255, 154, 56, 1)',
+                    ],
+    
+                },
+                {                
+                    label: 'Earned',
+                    data: [425, 180, 300, 275, 450, 100, 200, 500, 300],
+                    backgroundColor: [
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                        'rgba(89, 182, 109, 1)',
+                    ],
+    
+                }
+            ]
             },
             options: {
                 scales: {
@@ -69,15 +102,42 @@ $( function() {
     }
 
     // modal init
-    var modalBtn = $('.links .btn');
-    modalBtn.on('click', function (event) {
+    var modalBtns = $('[data-modal]');
+    modalBtns.on('click', function (event) {
         event.preventDefault();
-
-        $('.modal').bPopup();
-    })
-    // formstyler
-    $(function() {
-        //   $('select').styler();
-        
+        var target = $(this).attr("data-modal");
+        $(target).bPopup({
+            closeClass: 'close-modal-btn'
         });
+    });
+
+    // scrollbar init 
+    $('.scrollbar-inner').scrollbar();
+    
+    // formstyler init
+
+    $('select').styler();
+      
+
+    //range slider init
+    var handle = $('#custom-handle');
+    var range = $('#slider');
+        range.slider({
+            value: 50 ,
+            orientation: "horizontal",
+            range: "min",
+            animate: true,
+            max: 168,
+
+            create: function(event, ui) {
+                handle.append('<span class="value">'+$( this ).slider( "value" ) + 'hours</span>');
+                var value = handle.find('.value');
+                range.slider('option', 'valueSpan', value);
+            },
+            slide: function( event, ui ) {
+                range.slider( "option" ,"valueSpan").text(ui.value + 'hours');
+            }
+
+        });
+
   } );
